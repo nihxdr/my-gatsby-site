92e0907b2cebce1798855c63bde857c84a7ff8e3
Nihad <nihadrahim99@gmail.com>
2021-09-11 17:27:08 +0530

    ✨ finished Gatsby Tutorial Part-2
    - linked layout component to pages
    - styled the whole thing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
     return (
-        <main>
-            <title>Home Page</title>
-            <h1>Welcome to my Gatsby site!</h1>
-            <Link to="/about">About</Link>
-            <p>I'm making this by following the Gatsby Tutorial.</p>
-        </main>
+        <Layout pageTitle="Home Page">
+            <main>
+                <h3>Welcome to my Gatsby site!</h3>
+                <p>I'm making this by following the Gatsby Tutorial.</p>
+            </main>
+        </Layout>
     )
 }
 
